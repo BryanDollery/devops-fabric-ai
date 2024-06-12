f9c161a21104760cc5982d2e92561fe7692fb518
Bryan Dollery <bryan.dollery@gmail.com>
2024-06-12 09:37:36 +0300

    didn't need to pass the function name to _fabric function

--- functions.sh
+++ functions.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+############################################################
+# Wrapper around fabric that invokes it then saves/caches
+# responses in your 2nd brain. This is quite useful when
+# generating scripts, because you can examine the script
+# before actually executing it, or you can run it twice
+############################################################
+
 echo "Configuring fabric devops functions"
 
+# It's pretty important that this dir exists
+mkdir -p $BRAIN/auto
+
+# Just to make things prettier
 declare -A icons
-icons[research]="🔍"
+icons[research]="🔍" |
 icons[script]="💻"
 icons[shortcuts]="⌨️"
 icons[tools]="💡"
@@ -13,7 +24,9 @@ icons[ask]="❓"
 icons[whatis]="❓"
 icons[qq]="❓"
 
+# Invokes fabric and caches its responses in your brain
 function _fabric() {
+  # all the variables
   local class=$1
   local prompt="$2"
   local ts=$(date +"%Y-%m-%d-%H-%M-%S")
@@ -21,11 +34,13 @@ function _fabric() {
   local output="$name-output"
   local outputFile="$output.md"
   local mdFile="$name.md"
-  local digestFile="$BRAIN/auto/$(date +"%Y-%m-%d")-digest.md"
-  local res=$(fabric -p devops/$class --model "gpt-4o" --text "$prompt")
-  local summary=$(fabric -p summarize_micro --model "gpt-4o" --text "$prompt")
+  local digestFile="$BRAIN/auto/$(date +"%Y-%m-%d")-digest.md" # To keep a log of what we generated today
+  local res=$(fabric -p devops/$class --model "gpt-4o" --text "$prompt") # This is where we do what was asked of us
+  local summary=$(fabric -p summarize_micro --model "gpt-4o" --text "$prompt") # But fabric is so useful we can call it twice
+
+  # Write the files
   echo "$res" > "$BRAIN/auto/$outputFile"
-  echo " - $ts - [$class](./$mdFile)" >> "$digestFile"
+  echo " - [$ts - $class](./$mdFile): $prompt" >> "$digestFile"
   cat <<EOF > "$BRAIN/auto/$mdFile"
 ---
 title: "${icons[$class]} $class"
@@ -48,40 +63,41 @@ echo "$res"
 }
 
 function research() {
-  _fabric 'research' "$1"
+  _fabric $FUNCNAME "$1"
 }
 
 function script() {
-  _fabric 'script'  "$1"
+  _fabric $FUNCNAME "$1"
 }
 
 function shortcuts() {
-  _fabric 'shortcuts'  "$1"
+  _fabric $FUNCNAME "$1"
 }
 
 function tools() {
-  _fabric 'tools'  "$1"
+  _fabric $FUNCNAME "$1"
 }
 
 function bug() {
-  _fabric 'bug'  "$1"
+  _fabric $FUNCNAME "$1"
 }
 
 function installit() {
-  _fabric 'installit'  "$1"
+  _fabric $FUNCNAME "$1"
 }
 
 function ask() {
-  _fabric 'ask' "$1"
+  _fabric $FUNCNAME "$1"
 }
 
 function whatis() {
-  _fabric 'whatis' "$1"
+  _fabric $FUNCNAME "$1"
 }
 
 function qq() {
-  _fabric 'qq' "$1"
+  _fabric $FUNCNAME "$1"
 }
+
 
 export -f research
 export -f script
